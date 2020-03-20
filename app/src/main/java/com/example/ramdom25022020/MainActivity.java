24457a042bb8a247623e9b8a40b2bf39dbd61ec4
unknown <nucekm@gmail.com>
2020-03-20 14:09:21 +0700

    giao diện game loto

--- app/src/main/java/com/example/ramdom25022020/MainActivity.java
+++ app/src/main/java/com/example/ramdom25022020/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.ramdom25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
